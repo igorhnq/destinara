0a85413e64477c603dfa6dda26982d3f52bb5be2
igorhnq <igorhenrique102@gmail.com>
2024-11-10 20:16:44 -0300

    feat: create TravelPackage model

---
+++ src/main/java/br/com/destinara/model/TravelPackage.java
@@ -0,0 +1,25 @@
+package br.com.destinara.model;
+
+import java.math.BigDecimal;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "travel_package")
+public class TravelPackage {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String name;
+    private String type;
+    private BigDecimal price;
+    private String description;
+}
