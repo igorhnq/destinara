c426b485d52978497d326685a62636375ec5f8ba
igorhnq <igorhenrique102@gmail.com>
2024-11-10 21:29:06 -0300

    feat: add filter by package type

--- src/main/java/br/com/destinara/controller/TravelPackageController.java
+++ src/main/java/br/com/destinara/controller/TravelPackageController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import br.com.destinara.model.TravelPackageModel;
 import br.com.destinara.repository.TravelPackageRepository;
@@ -22,6 +23,13 @@ public class TravelPackageController {
     public String showPackages(Model model) {
         List<TravelPackageModel> travelPackageModels = travelPackageRepository.findAll();
         model.addAttribute("travelPackages", travelPackageModels);
+        return "index";
+    }
+
+    @GetMapping("/filter")
+    public String filterByType(@RequestParam String type, Model model) {
+        List<TravelPackageModel> filteredPackages = travelPackageRepository.findByType(type);
+        model.addAttribute("travelPackages", filteredPackages);
         return "index";
     }
 }
--- src/main/java/br/com/destinara/repository/TravelPackageRepository.java
+++ src/main/java/br/com/destinara/repository/TravelPackageRepository.java
@@ -1,9 +1,11 @@
 package br.com.destinara.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import br.com.destinara.model.TravelPackageModel;
 
 public interface TravelPackageRepository extends JpaRepository <TravelPackageModel, Integer> {
-        
+    List<TravelPackageModel> findByType(String type);
 }
