eb6065641bbbd133e05e0aed1f554698a59ce817
igorhnq <igorhenrique102@gmail.com>
2024-11-15 19:50:06 -0300

    feat: create AppUser repository

---
+++ src/main/java/br/com/destinara/repository/AppUserRepository.java
@@ -0,0 +1,11 @@
+package br.com.destinara.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import br.com.destinara.model.AppUserModel;
+
+public interface AppUserRepository extends JpaRepository<AppUserModel, Integer> {
+    Optional<AppUserModel> findByEmail(String email);
+} 
