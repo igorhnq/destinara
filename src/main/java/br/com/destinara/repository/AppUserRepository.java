f1e12f799f11893555627311445ffbe163eccd4b
igorhnq <igorhenrique102@gmail.com>
2024-11-15 21:41:41 -0300

    fix: add password param

--- src/main/java/br/com/destinara/repository/AppUserRepository.java
+++ src/main/java/br/com/destinara/repository/AppUserRepository.java
@@ -7,5 +7,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import br.com.destinara.model.AppUserModel;
 
 public interface AppUserRepository extends JpaRepository<AppUserModel, Integer> {
-    Optional<AppUserModel> findByEmail(String email);
+    Optional<AppUserModel> findByEmailAndPassword(String email, String password);
 } 
