fe60b4a93883057891f552d1ab8948048d8b633e
igorhnq <igorhenrique102@gmail.com>
2024-11-24 18:16:31 -0300

    feat: enable email editing and update user authentication

--- src/main/java/br/com/destinara/controller/AppUserController.java
+++ src/main/java/br/com/destinara/controller/AppUserController.java
@@ -1,6 +1,8 @@
 package br.com.destinara.controller;
 
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
@@ -56,21 +58,32 @@ public class AppUserController {
         return "redirect:/purchase-history";
     }
 
-    @PostMapping("/update-user")
+     @PostMapping("/update-user")
     public String updateUser(AppUserModel updatedUser) {
+        UserDetails userDetails = (UserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        AppUserModel currentUser = appUserRepository.findByEmail(userDetails.getUsername()).orElse(null);
 
-    UserDetails userDetails = (UserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-    AppUserModel currentUser = appUserRepository.findByEmail(userDetails.getUsername()).orElse(null);
+        if (currentUser != null) {
+            currentUser.setName(updatedUser.getName());
+            currentUser.setEmail(updatedUser.getEmail());
+            currentUser.setCpf(updatedUser.getCpf());
+            currentUser.setBirthDate(updatedUser.getBirthDate());
+            currentUser.setSex(updatedUser.getSex());
 
-    if (currentUser != null) {
-        currentUser.setName(updatedUser.getName());
-        currentUser.setEmail(updatedUser.getEmail());
-        currentUser.setCpf(updatedUser.getCpf());
-        currentUser.setBirthDate(updatedUser.getBirthDate());
-        currentUser.setSex(updatedUser.getSex());
+            appUserRepository.save(currentUser);
 
-        appUserRepository.save(currentUser);
-    }
+            User updatedUserDetails = new User(
+                currentUser.getEmail(), 
+                currentUser.getPassword(),
+                userDetails.getAuthorities()
+            );
+            UsernamePasswordAuthenticationToken newAuth = new UsernamePasswordAuthenticationToken(
+                updatedUserDetails, 
+                null, 
+                updatedUserDetails.getAuthorities()
+            );
+            SecurityContextHolder.getContext().setAuthentication(newAuth);
+        }
         return "redirect:/purchase-history"; 
     }
 }
