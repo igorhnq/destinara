fbf587a468e9ea3a61756e90fc5c4243da757e08
igorhnq <igorhenrique102@gmail.com>
2024-11-24 02:43:16 -0300

    feat: add finByUser method

--- src/main/java/br/com/destinara/repository/PurchaseRepository.java
+++ src/main/java/br/com/destinara/repository/PurchaseRepository.java
@@ -1,7 +1,11 @@
 package br.com.destinara.repository;
 
+import br.com.destinara.model.AppUserModel;
 import br.com.destinara.model.PurchaseModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface PurchaseRepository extends JpaRepository<PurchaseModel, Integer> {
+    List<PurchaseModel> findByUser(AppUserModel user);
 }
