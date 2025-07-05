3c22c6ad57e60a22635ac5fc3c29a1e621ff5205
igorhnq <igorhenrique102@gmail.com>
2025-07-05 19:03:07 -0300

    feat: add borderColor and textColor props

--- frontend/src/components/Button/index.tsx
+++ frontend/src/components/Button/index.tsx
@@ -1,10 +1,14 @@
 interface ButtonProps {
     children: React.ReactNode;
+    borderColor?: string;
+    textColor?: string;  
 }
 
-export default function Button({ children }: ButtonProps) {
+export default function Button({ children, borderColor = "border-white", textColor = "text-white" }: ButtonProps) {
     return (
-        <button className="flex items-center justify-center border-white text-white text-xl font-bold border-4 rounded-3xl p-2 w-60 hover:cursor-pointer">
+        <button
+            className={`flex items-center justify-center ${borderColor} ${textColor} text-xl font-bold border-4 rounded-3xl p-2 w-60 hover:cursor-pointer`}
+        >
             {children}
         </button>
     );
