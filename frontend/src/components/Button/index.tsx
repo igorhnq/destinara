c40a52870d5265969e5e2e640d3c0211b701f2ef
igorhnq <igorhenrique102@gmail.com>
2025-07-06 14:54:06 -0300

    feat: add width and background color props

--- frontend/src/components/Button/index.tsx
+++ frontend/src/components/Button/index.tsx
@@ -1,13 +1,21 @@
 interface ButtonProps {
     children: React.ReactNode;
     borderColor?: string;
-    textColor?: string;  
+    textColor?: string;
+    width?: string;
+    backgroundColor?: string;
 }
 
-export default function Button({ children, borderColor = "border-white", textColor = "text-white" }: ButtonProps) {
+export default function Button({
+    children,
+    borderColor = "border-white",
+    textColor = "text-white",
+    width = "w-60",
+    backgroundColor = ""
+}: ButtonProps) {
     return (
         <button
-            className={`flex items-center justify-center ${borderColor} ${textColor} text-xl font-bold border-4 rounded-3xl p-2 w-60 hover:cursor-pointer`}
+            className={`flex items-center justify-center ${borderColor} ${textColor} text-xl font-bold border-4 rounded-3xl p-2 ${width} ${backgroundColor} hover:cursor-pointer`}
         >
             {children}
         </button>
