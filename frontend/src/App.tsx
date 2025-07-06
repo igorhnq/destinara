aecb7b310fa9bd4833fe66eda193ef8004ca5836
igorhnq <igorhenrique102@gmail.com>
2025-07-06 14:53:50 -0300

    feat: add travel packages page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Welcome from './pages/Welcome'
 import Login from './pages/Login'
 import Register from './pages/Register'
+import TravelPackages from './pages/TravelPackages'
 
 export default function App() {
 	return (
@@ -10,6 +11,7 @@ export default function App() {
 				<Route path="/" element={<Welcome />} />
 				<Route path="/login" element={<Login />} />
 				<Route path="/register" element={<Register />} />
+				<Route path="/travel-packages" element={<TravelPackages />} />
 			</Routes>
 		</Router>
 	)
---
+++ frontend/src/pages/TravelPackages/index.tsx
@@ -0,0 +1,30 @@
+import Button from "../../components/Button";
+import Card from "../../components/Card";
+import Header from "../../components/Header";
+import DefaultLayout from "../../layouts/DefaultLayout";
+import manausImage from "../../assets/images/manaus-card.svg";
+import fozImage from "../../assets/images/foz-do-iguacu-card.svg";
+import salvadorImage from "../../assets/images/salvador-card.svg";
+import gramadoImage from "../../assets/images/gramado-card.svg";
+
+export default function TravelPackages() {
+    return (
+        <DefaultLayout flexCol innerPadding="p-16">
+            <Header />
+            <div className="flex justify-end gap-3 mt-28">
+                <Button textColor="text-[#5BB8BB]" borderColor="border-[#5BB8BB]">
+                    NACIONAIS
+                </Button>
+                <Button textColor="text-[#5BB8BB]" borderColor="border-[#5BB8BB]">
+                    INTERNACIONAIS
+                </Button>
+            </div>
+            <div className="flex gap-8 mt-10 flex-1 w-full">
+                <Card name="Manaus, AM" image={manausImage} />
+                <Card name="Foz do Iguaçu, PR" image={fozImage} />
+                <Card name="Salvador, BA" image={salvadorImage} />
+                <Card name="Gramado, RS" image={gramadoImage} />
+            </div>
+        </DefaultLayout>
+    )
+}
