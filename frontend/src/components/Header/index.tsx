8f1dfc11418909ba8cf7ada0989af0e69a498052
igorhnq <igorhenrique102@gmail.com>
2025-07-08 21:01:57 -0300

    feat: add isPackageDetails prop to header

--- frontend/src/components/Header/index.tsx
+++ frontend/src/components/Header/index.tsx
@@ -1,11 +1,24 @@
 import { UserIcon } from "@phosphor-icons/react";
 
-export default function Header() {
+interface HeaderProps {
+    isPackageDetails?: boolean;
+    packageName?: string;
+}
+
+export default function Header({ isPackageDetails = false, packageName }: HeaderProps) {
     return (
         <header className="flex items-center justify-between w-full">
-            <div className="flex items-center gap-5">
+            <div className="flex items-center gap-5 h-[75px]">
                 <div className="flex items-center justify-center border-[#5BB8BB] border-2 w-[75px] h-[75px]">logo</div>
-                <h1 className="text-[#5BB8BB] text-5xl font-bold">PACOTE DE VIAGENS</h1>
+                {!isPackageDetails && (
+                    <h1 className="text-[#5BB8BB] text-5xl font-bold">PACOTE DE VIAGENS</h1>
+                )}
+                {isPackageDetails && packageName && (
+                    <div className="flex flex-col gap-2">
+                        <h1 className="text-[#5BB8BB] text-5xl font-bold">{packageName}</h1>
+                        <p className="text-[#939090] text-sm">Manaus é o principal ponto de entrada para a Amazônia, oferecendo uma mistura de cultura indígena e belezas naturais. </p>
+                    </div>
+                )}
             </div>
             <UserIcon size={42} color="#777777" weight="bold" />
         </header>
